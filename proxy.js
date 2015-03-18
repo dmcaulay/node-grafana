47cb139ce46e3cb3c337c462f58106f2b836c459
Dan McAulay <dmcaulay@gmail.com>
2015-03-18 12:14:11 -0700

    remove global

--- proxy.js
+++ proxy.js
@@ -6,7 +6,7 @@ var pathToRegexp = require('path-to-regexp');
 module.exports = function(options) {
   var regex = pathToRegexp(options.path + '/:path+');
   return function(req, res, next) {
-    m = req.url.match(regex);
+    var m = req.url.match(regex);
     if (m) {
       req.url = '/' + m[1];
       proxy.web(req, res, { target: options.target });
